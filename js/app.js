01a6234a25f78ed0247a52610b396d31472a13ec
bustosjeiner <bustosjeiner@gmail.com>
2021-11-30 01:16:40 -0500

    description secction

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function seeMore(){
+    document.getElementById('more-text').style.display = 'block';
+    document.getElementById('btn-see-more').style.display = 'none';
+}
+
+let mainPosition = window.pageYOffset; 
+let $nav = document.querySelector("nav-bar");
+
+window.addEventListener("scroll", () => {
+    let actualMovement = window.pageYOffset;
+
+    if(mainPosition >= actualMovement) {
+        $nav.style.top = "0px";
+    } 
+    else {
+        $nav.style.top = "-60px";
+    }
+    mainPosition = actualMovement;
+});
